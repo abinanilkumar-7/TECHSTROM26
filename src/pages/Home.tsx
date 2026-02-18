ac8d2364e62e7cf2e2e8577662417a0d724c3d3b
Abin Anilkumar <140091592+abinanilkumar-7@users.noreply.github.com>
2026-02-18 14:12:37 +0530

    Update Instagram link in MagicalButton component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -40,7 +40,7 @@ const Home = () => {
                     <Countdown />
 
                     <div className="cta-container" data-aos="fade-up" data-aos-delay="800">
-                        <MagicalButton href="https://www.instagram.com/veltech_multitech1999?igsh=bzBvZGx2Z2pya3Fi">
+                        <MagicalButton href="https://www.instagram.com/veltech_multitech1999?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">
                             Enter the Academy
                         </MagicalButton>
                     </div>
